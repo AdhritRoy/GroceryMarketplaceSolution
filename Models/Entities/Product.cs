aa44350d44bf8e997900a6ef918518403e0c6c77
AdhritRoy <nsroy27@outlook.com>
2026-03-24 22:49:52 +0530

    Create Product.cs file in Models/Entities directory

---
+++ Models/Entities/Product.cs
@@ -0,0 +1,14 @@
+// Current Date and Time (UTC)
+// 2026-03-24 17:17:49
+// Current User's Login: AdhritRoy
+
+namespace Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
