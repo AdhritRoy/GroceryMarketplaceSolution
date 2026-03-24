6dbd06607459d6d586cfbb891a3f9fda22aa94c1
AdhritRoy <nsroy27@outlook.com>
2026-03-24 22:57:16 +0530

    Create Quote.cs file in Models/Entities directory

---
+++ Models/Entities/Quote.cs
@@ -0,0 +1,15 @@
+// This file was generated automatically
+//
+// Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2026-03-24 17:17:50
+// Current User's Login: AdhritRoy
+
+namespace Models.Entities
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
